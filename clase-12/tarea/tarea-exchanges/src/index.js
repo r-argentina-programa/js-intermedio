4b081ba16faf6d9ec9f1cbe3ad8f8910581d0b91
black753 <mauroo_00@hotmail.com>
2020-01-24 17:30:25 -0300

    Finalizacion de la pagina
    Agregue validacion de inputs y manejo de errores de las promesas

--- clase-12/tarea/tarea-exchanges/src/index.js
+++ clase-12/tarea/tarea-exchanges/src/index.js
@@ -4,6 +4,62 @@ fetch("https://api.exchangeratesapi.io/latest")
     .then(respuestaJSON => añadirBases(respuestaJSON))
     
 
+
+
+document.querySelector("#convertir").onclick = ()=>{
+    document.querySelector("#devolucion-monedas").innerHTML=""
+
+    const fecha=document.querySelector("#fecha").value
+    const monedaBase=document.querySelector("#listado-bases").value
+
+    const url=`https://api.exchangeratesapi.io/${fecha}?base=${monedaBase}`
+
+    if(validarInputs(fecha, monedaBase)){
+
+        if(document.querySelector("#datos-incorrectos")){
+            document.querySelector("#datos-incorrectos").remove()
+        }
+
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(respuestaJSON => {
+                debugger
+
+                Object.keys(respuestaJSON.rates).forEach(moneda => {
+                    const li=document.createElement("li")
+                    li.innerText=`${moneda}: ${respuestaJSON.rates[moneda]}`
+
+
+                    document.querySelector("#devolucion-monedas").appendChild(li)
+                })
+            })
+            .catch(error => {
+ 
+                const alerta=document.createElement("div")
+                alerta.className="alert alert-danger"
+                alerta.role="alert"
+                alerta.id="datos-incorrectos"
+                alerta.textContent=`No pudimos obtener los datos de esa fecha y con esa base. Lo sentimos`
+        
+                document.querySelector("form").appendChild(alerta)
+                 
+
+            })
+    }else{
+        if(!document.querySelector("#datos-incorrectos")){
+            const alerta=document.createElement("div")
+            alerta.className="alert alert-danger"
+            alerta.role="alert"
+            alerta.id="datos-incorrectos"
+            alerta.textContent="Los datos ingresados son incorrectos. Intentelo nuevamente"
+
+            document.querySelector("form").appendChild(alerta)
+        } 
+    }
+        
+        
+}
+
 function añadirBases(respuestaJSON){
     const lista = document.querySelector("#listado-bases")
 
@@ -23,26 +79,8 @@ function añadirBases(respuestaJSON){
     })
 }
 
-document.querySelector("#convertir").onclick = ()=>{
-    document.querySelector("#devolucion-monedas").innerHTML=""
-
-    const fecha=document.querySelector("#fecha").value
-    const monedaBase=document.querySelector("#listado-bases").value
-
-    const url=`https://api.exchangeratesapi.io/${fecha}?base=${monedaBase}`
-
-
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(respuestaJSON => {
-
-            Object.keys(respuestaJSON.rates).forEach(moneda => {
-                const li=document.createElement("li")
-                li.innerText=`${moneda}: ${respuestaJSON.rates[moneda]}`
-
-
-                document.querySelector("#devolucion-monedas").appendChild(li)
-            })
-        })
-        
+function validarInputs(fecha, moneda){
+    if(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/.test(fecha) && moneda.length===3){
+        return true
+    }
 }
